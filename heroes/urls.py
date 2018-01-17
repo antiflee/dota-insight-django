7223f26799725052a4bbdc848dfc77e1e41df1ba
Yufei Liu <yl@Yufeis-MacBook-Air.local>
2018-01-17 15:42:01 -0800

    implement updating win rate from redis

--- heroes/urls.py
+++ heroes/urls.py
@@ -6,4 +6,5 @@ app_name = 'heroes'
 
 urlpatterns = [
     url(r'^$', views.heroes, name='heroesHome'),
+    url(r'^winrate$', views.getHeroesWinRate, name='heroesWinRate'),
 ]
--- heroes/views.py
+++ heroes/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
 from .models import Hero
 
@@ -8,3 +9,29 @@ def heroes(request):
     heroes = Hero.objects.all()
 
     return render(request, 'heroes/heroesHome.html', {'heroes':heroes})
+
+
+
+# Get the win rate for all heroes. Return a json file.
+
+import redis
+import os
+import json
+
+def getHeroesWinRate(request):
+
+    # HOSTURL = os.environ.get('AWS_REDIS_URL')
+    # print(HOSTURL)
+    HOSTURL = "ec2-34-213-4-249.us-west-2.compute.amazonaws.com"
+    r = redis.StrictRedis(host=HOSTURL, port=6379, db=0)
+
+    res = {}
+
+    for heroId in range(1, 120):
+        winRate = r.get(heroId)
+        if winRate:
+            res[heroId] = winRate.decode("utf-8")
+
+    resJson = json.dumps(res)
+
+    return JsonResponse(resJson,safe=False)
